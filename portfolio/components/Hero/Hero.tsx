70d1e811d4eb08bc614487d143c53ae8d4481b38
3liantte <eliantte49@gmail.com>
2025-02-03 21:26:55 +0200

    portfolio update

--- portfolio/components/Hero/Hero.tsx
+++ portfolio/components/Hero/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Spotlight } from '../ui/Spotlight'
 import { TextGenerateEffect } from '../ui/TextGenerateEffect'
+import MagicButton from '../ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -26,8 +28,16 @@ const Hero = () => {
             />
 
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-                Hi i&apos;m Eliantte, Web Dev
+                Hi i&apos;m Eliantte, Web and Mobile Application Developer.
             </p>
+
+            <a href="#about">
+              <MagicButton
+                title="Browse my work"
+                icon={<FaLocationArrow/>}
+                position='right'
+              />
+            </a>
         </div>
     </div>
 
